cb0b8fd4514934a1a803b15669f4abba803751f8
Aleksandr Kornjushko <aleksandr.kornjushko@coolbet.com>
2023-08-31 15:35:29 +0300

    add a delete todo test

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -19,3 +19,29 @@ test("should be able to add a new todo", async ({ page }) => {
     const todoItem = page.locator('[data-testid=todo-item]');
     expect(await todoItem.innerText()).toEqual('Learn Playwright');
 });
+
+test("should be able to delete a todo", async ({ page }) => {
+    await page.goto('https://todo.qacart.com/signup');
+    await page.type('[data-testid=first-name]', 'QAcart');
+    await page.type('[data-testid=last-name]', 'Awesome!');
+    await page.type('[data-testid=email]', 'test223@example.com');
+    await page.type('[data-testid=password]', 'Test1234');
+    await page.type('[data-testid=confirm-password]', 'Test1234');
+    await page.click('[data-testid=submit]');
+
+    const welcomeMessage = page.locator('[data-testid=welcome]');
+    await expect(welcomeMessage).toBeVisible();
+
+    await page.click('[data-testid=add]');
+    await page.type('[data-testid=new-todo]', 'Learn Playwright');
+    await page.click('[data-testid=submit-newTask]');
+
+    const todoItem = page.locator('[data-testid=todo-item]');
+    expect(await todoItem.innerText()).toEqual('Learn Playwright');
+
+    await page.click('[data-testid=delete]');
+
+    const noTodosMessage = page.locator('[data-testid=no-todos]');
+    await expect(noTodosMessage).toBeVisible();
+
+});
