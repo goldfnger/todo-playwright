ecc018a50d2fa14ecfccd5607b15da4b58843731
Aleksandr Kornjushko <aleksandr.kornjushko@coolbet.com>
2023-09-04 17:08:40 +0300

    replace user data with User model

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,13 +1,19 @@
-import { test, expect } from '@playwright/test'
-import { faker } from '@faker-js/faker'
-
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+import User from '../models/User';
 test("should be able to register to our application", async ({ page }) => {
+    const user = new User(
+        faker.person.firstName(),
+        faker.person.lastName(),
+        faker.internet.email(),
+        'Test1234');
+
     await page.goto('/signup');
-    await page.type('[data-testid=first-name]', faker.person.firstName());
-    await page.type('[data-testid=last-name]', faker.person.lastName());
-    await page.type('[data-testid=email]', faker.internet.email());
-    await page.type('[data-testid=password]', 'Test1234');
-    await page.type('[data-testid=confirm-password]', 'Test1234');
+    await page.type('[data-testid=first-name]', user.getFirstName());
+    await page.type('[data-testid=last-name]', user.getLastName());
+    await page.type('[data-testid=email]', user.getEmail());
+    await page.type('[data-testid=password]', user.getPassword());
+    await page.type('[data-testid=confirm-password]', user.getPassword());
     await page.click('[data-testid=submit]');
     const welcomeMessage = page.locator('[data-testid=welcome]');
     await expect(welcomeMessage).toBeVisible();
