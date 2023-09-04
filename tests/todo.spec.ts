4600b16d192d61adf259e9b75ad2febedfb2949a
Aleksandr Kornjushko <aleksandr.kornjushko@coolbet.com>
2023-09-04 16:51:36 +0300

    Add todo using API call

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -44,28 +44,51 @@ test("should be able to add a new todo", async ({ page, request, context }) => {
     expect(await todoItem.innerText()).toEqual('Learn Playwright');
 });
 
-test("should be able to delete a todo", async ({ page }) => {
-    await page.goto('/signup');
-    await page.type('[data-testid=first-name]', faker.person.firstName());
-    await page.type('[data-testid=last-name]', faker.person.lastName());
-    await page.type('[data-testid=email]', faker.internet.email());
-    await page.type('[data-testid=password]', 'Test1234');
-    await page.type('[data-testid=confirm-password]', 'Test1234');
-    await page.click('[data-testid=submit]');
+test("should be able to delete a todo", async ({ page, request, context }) => {
+    const response = await request.post('/api/v1/users/register', {
+        data: {
+            email: faker.internet.email(),
+            password: "Test1234",
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName()
+        },
+    });
 
-    const welcomeMessage = page.locator('[data-testid=welcome]');
-    await expect(welcomeMessage).toBeVisible();
+    const responseBody = await response.json();
+    const accessToken = responseBody.access_token;
+    const firstName = responseBody.firstName;
+    const userID = responseBody.userID;
 
-    await page.click('[data-testid=add]');
-    await page.type('[data-testid=new-todo]', 'Learn Playwright');
-    await page.click('[data-testid=submit-newTask]');
+    await context.addCookies([
+        {
+            name: 'access_token',
+            value: accessToken,
+            url: 'https://todo.qacart.com'
+        },
+        {
+            name: 'firstName',
+            value: firstName,
+            url: 'https://todo.qacart.com'
+        },
+        {
+            name: 'userID',
+            value: userID,
+            url: 'https://todo.qacart.com'
+        }
+    ]);
 
-    const todoItem = page.locator('[data-testid=todo-item]');
-    expect(await todoItem.innerText()).toEqual('Learn Playwright');
+    await request.post('/api/v1/tasks', {
+        data: {
+            isCompleted: false,
+            item: "Learn Playwright",
+        },
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    });
 
+    await page.goto('/todo');
     await page.click('[data-testid=delete]');
-
     const noTodosMessage = page.locator('[data-testid=no-todos]');
     await expect(noTodosMessage).toBeVisible();
-
 });
