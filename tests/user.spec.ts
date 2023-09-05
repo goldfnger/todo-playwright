a85d835688a9c890b94899f6f1cb38a74686a199
Aleksandr Kornjushko <aleksandr.kornjushko@coolbet.com>
2023-09-05 14:40:35 +0300

    refactor to use Signup POM

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,15 +1,13 @@
 import { test, expect } from '@playwright/test';
 import User from '../models/User';
+import SignupPage from '../pages/SignupPage';
+
 test("should be able to register to our application", async ({ page }) => {
     const user = new User();
+    const signupPage = new SignupPage();
 
-    await page.goto('/signup');
-    await page.type('[data-testid=first-name]', user.getFirstName());
-    await page.type('[data-testid=last-name]', user.getLastName());
-    await page.type('[data-testid=email]', user.getEmail());
-    await page.type('[data-testid=password]', user.getPassword());
-    await page.type('[data-testid=confirm-password]', user.getPassword());
-    await page.click('[data-testid=submit]');
+    await signupPage.load(page);
+    await signupPage.signup(page, user);
     const welcomeMessage = page.locator('[data-testid=welcome]');
     await expect(welcomeMessage).toBeVisible();
 });
