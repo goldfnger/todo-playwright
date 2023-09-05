e7e351ef5cd2e8989cc26845a26cb946351e8642
Aleksandr Kornjushko <aleksandr.kornjushko@coolbet.com>
2023-09-05 14:47:40 +0300

    refactor to use Todo POM

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -1,13 +1,16 @@
 import { test, expect } from '@playwright/test';
 import User from '../models/User';
 import SignupPage from '../pages/SignupPage';
+import TodoPage from '../pages/TodoPage';
 
 test("should be able to register to our application", async ({ page }) => {
     const user = new User();
     const signupPage = new SignupPage();
+    const todoPage = new TodoPage();
 
     await signupPage.load(page);
     await signupPage.signup(page, user);
-    const welcomeMessage = page.locator('[data-testid=welcome]');
+
+    const welcomeMessage = todoPage.getWelcomeMessageElement(page);
     await expect(welcomeMessage).toBeVisible();
 });
