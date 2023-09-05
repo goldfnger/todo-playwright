af69193fcc375fd7f29201d4cd1416136f315088
Aleksandr Kornjushko <aleksandr.kornjushko@coolbet.com>
2023-09-05 15:19:24 +0300

    move todo logic from test file to TodoPage

--- pages/TodoPage.ts
+++ pages/TodoPage.ts
@@ -5,7 +5,27 @@ export default class TodoPage {
         return '[data-testid=welcome]'
     }
 
+    private get deleteIcon() {
+        return '[data-testid=delete]'
+    }
+
+    private get noTodosMessage() {
+        return '[data-testid=no-todos]'
+    }
+
+    async load(page: Page) {
+        await page.goto('/todo');
+    }
+
     getWelcomeMessageElement(page: Page) {
         return page.locator(this.welcomeMessage);
+    }
+
+    async deleteTodo(page: Page) {
+        await page.click(this.deleteIcon);
+    }
+
+    async getNoTodosMessage(page: Page) {
+        return page.locator(this.noTodosMessage);
     }
 }
