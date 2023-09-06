47cae8d56e546e0656736ed40d22d54d72218e94
Aleksandr Kornjushko <aleksandr.kornjushko@coolbet.com>
2023-09-06 17:35:51 +0300

    refactor todo tests with NewTodoPage and TodoPage POM's

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,22 +1,23 @@
 import { test, expect } from "@playwright/test";
 import User from "../models/User";
-import UserAPI from "../APIs/UserAPI";
-import TodoAPI from "../APIs/TodoAPI";
 import SignupPage from "../pages/SignupPage";
 import TodoPage from "../pages/TodoPage";
+import NewTodoPage from "../pages/NewTodoPage";
 
 test("should be able to add a new todo", async ({ page, request, context }) => {
     const user = new User();
     const signupPage = new SignupPage();
+    const newTodoPage = new NewTodoPage();
+    const todoPage = new TodoPage();
 
     await signupPage.signupByAPI(request, user, context);
 
-    await page.goto('/todo');
-    await page.click('[data-testid=add]');
-    await page.type('[data-testid=new-todo]', 'Learn Playwright');
-    await page.click('[data-testid=submit-newTask]');
+    await newTodoPage.load(page);
+    await newTodoPage.addTodo(page, 'Learn Playwright');
 
-    const todoItem = page.locator('[data-testid=todo-item]');
+    await todoPage.getTodoItem(page);
+    const todoItem = await todoPage.getTodoItem(page);
+
     expect(await todoItem.innerText()).toEqual('Learn Playwright');
 });
 
@@ -24,9 +25,10 @@ test("should be able to delete a todo", async ({ page, request, context }) => {
     const user = new User();
     const signupPage = new SignupPage();
     const todoPage = new TodoPage();
+    const newTodoPage = new NewTodoPage();
 
     await signupPage.signupByAPI(request, user, context);
-    await new TodoAPI().addTodo(request, user);
+    await newTodoPage.addTodoAPI(request, user);
 
     await todoPage.load(page);
     await todoPage.deleteTodo(page);
